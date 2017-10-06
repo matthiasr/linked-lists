6a40b56d384f05463451773804be8556134148b5
Matthias Rampke <matthias@rampke.de>
2017-10-06 18:11:08 +0200

    start implementing a max-depth search

--- recursion/recursion.go
+++ recursion/recursion.go
@@ -1,8 +1,36 @@
-package cons
+package main
+
+import (
+	"fmt"
+	"log"
+)
 
 func rec(n uint64) error {
 	if n == 0 {
 		return nil
 	}
 	return rec(n - 1)
+}
+
+func tryRec(n uint64, c chan<- error) {
+	defer func() {
+		if r := recover(); r != nil {
+			c <- fmt.Errorf("%v", r)
+		}
+	}()
+
+	c <- rec(n)
+}
+
+func main() {
+	c := make(chan error)
+	d := uint64(1e3)
+	log.Printf("trying recursion depth %d", d)
+	go tryRec(1000, c)
+	err := <-c
+	if err != nil {
+		log.Printf("recursion failed for depth: %v", d, err)
+	} else {
+		log.Printf("success for depth %v", d)
+	}
 }
--- recursion/recursion_test.go
+++ recursion/recursion_test.go
@@ -1,4 +1,4 @@
-package cons
+package main
 
 import (
 	"testing"
